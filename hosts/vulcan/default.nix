f27dda0d2c72036e9f213cc206da1a6324aa8c70
Addison Hernandez <addison.hernandez@gmail.com>
2024-08-12 15:57:30 -0400

    Add dual monitor display config for vulcan

--- hosts/vulcan/default.nix
+++ hosts/vulcan/default.nix
@@ -32,5 +32,14 @@
   nixpkgs.config.nvidia.acceptLicense = true;
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
 
+  # configure dual monitors
+  # HDMI: primary
+  # DP: secondary on the left and rotated with left edge up
+  services.xserver.displayManager.setupCommands = ''
+    xrandr \
+      --output DP-1 --mode 1080x1920 --pos 0x0 --rotate left \
+      --output HDMI-0 --primary --mode 1920x1080 --pos 1080x250
+  '';
+
   system.stateVersion = "24.05";
 }
