45a565e23b75ef1960d16e5e2dce971c8c90f7cd
Addison Hernandez <addison.hernandez@gmail.com>
2024-08-21 13:32:01 -0400

    (home global) remove duplicate wezterm declaration
    This is automatically included in the home-manager module.

--- home/addison/features/desktop/wezterm.nix
+++ home/addison/features/desktop/wezterm.nix
@@ -2,7 +2,6 @@
   programs.wezterm = {
     enable = true;
     extraConfig = ''
-      local wezterm = require 'wezterm';
       return {
         color_scheme = "Catppuccin Macchiato",
         font = wezterm.font("FiraCode Nerd Font"),
