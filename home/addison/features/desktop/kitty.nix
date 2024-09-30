a8506e2f8da97974b8ea87dee377224d951413c6
Addison Hernandez <addison.hernandez@gmail.com>
2024-09-30 15:52:41 -0400

    (addison@home) Additional kitty terminal config

--- home/addison/features/desktop/kitty.nix
+++ home/addison/features/desktop/kitty.nix
@@ -2,5 +2,34 @@
   programs.kitty = {
     enable = true;
     themeFile = "Catppuccin-Macchiato";
+
+    settings = {
+      enable_audio_bell = "no";
+      remember_window_size = "no";
+
+      initial_window_width = "100c";
+      initial_window_height = "36c";
+      window_padding_width = "3 5 0";
+
+      inactive_text_alpha = "0.25";
+      hide_window_decorations = "yes";
+      background_opacity = "0.95";
+
+      tab_bar_style = "powerline";
+      tab_powerline_style = "slanted";
+    };
+
+    extraConfig = ''
+      font_family      FiraCode Nerd Font Reg
+      bold_font        FiraCode Nerd Font SemBd
+      italic_font      Monaspace Radon Var Italic
+      bold_italic_font Monaspace Radon Var SemiBold Italic
+      font_size        11.0
+
+      font_features FiraCodeNF-Reg                    +calt +zero +cv16
+      font_features FiraCodeNF-SemBd                  +calt +zero +cv16
+      font_features MonaspaceRadonVar_400wght_-11slnt +calt +liga
+      font_features MonaspaceRadonVar_600wght_-11slnt +calt +liga
+    '';
   };
 }
