ae18960b237bc1d8404d7bbedb9eb93696ecdb32
Addison Hernandez <addison.hernandez@gmail.com>
2024-10-06 18:01:01 -0400

    (vulcan) Add nix-ld and modularize config

--- hosts/vulcan/default.nix
+++ hosts/vulcan/default.nix
@@ -8,26 +8,23 @@
     inputs.hardware.nixosModules.common-gpu-nvidia-nonprime
     ./hardware-configuration.nix
 
-    ./services
-
     ../common/global
     ../common/users/addison
 
     ../common/optional/docker.nix
     ../common/optional/flatpak.nix
+    ../common/optional/kde.nix
+    ../common/optional/nix-ld.nix
+    ../common/optional/pipewire.nix
+    ../common/optional/printing.nix
+    ../common/optional/systemd-boot.nix
+    ../common/optional/udisks.nix
+    ../common/optional/xserver.nix
   ];
 
   networking = {
     hostName = "vulcan";
     networkmanager.enable = true;
-  };
-
-  boot = {
-    loader = {
-      systemd-boot.enable = true;
-      efi.canTouchEfiVariables = true;
-    };
-    # kernelPackages = pkgs.linuxKernel.packages.linux_xanmod_latest;
   };
 
   nixpkgs.config.nvidia.acceptLicense = true;
