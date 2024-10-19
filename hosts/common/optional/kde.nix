0a72bf4244e62ec7857941daa677935319279db7
Addison Hernandez <addison.hernandez@gmail.com>
2024-10-19 13:52:49 -0400

    chore(all hosts) Remove konsole and elisa
    Konsole is made redundant by kitty and wezterm.
    Elisa (music player) is made redundant by VLC.

--- hosts/common/optional/kde.nix
+++ hosts/common/optional/kde.nix
@@ -1,4 +1,8 @@
-{
+{pkgs, ...}: {
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
+  environment.plasma6.excludePackages = with pkgs; [
+    konsole
+    elisa
+  ];
 }
