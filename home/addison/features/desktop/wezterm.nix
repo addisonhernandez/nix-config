d1ddbe9a3566192b600e37574ba40c5461ba8c55
Addison Hernandez <addison.hernandez@gmail.com>
2024-10-20 11:38:14 -0400

    fix(home/addison) Set WebGpu front end for fonts
    See: https://github.com/wez/wezterm/issues/5990

--- home/addison/features/desktop/wezterm.nix
+++ home/addison/features/desktop/wezterm.nix
@@ -2,10 +2,21 @@
   programs.wezterm = {
     enable = true;
     extraConfig = ''
-      return {
-        color_scheme = "Catppuccin Macchiato",
-        font = wezterm.font("FiraCode Nerd Font"),
+      local config = wezterm.config_builder()
+
+      config.color_scheme = "Catppuccin Macchiato"
+      config.font = wezterm.font_with_fallback {
+        {
+          family = "FiraCode Nerd Font",
+          harfbuzz_features = { "calt", "zero", "cv16" },
+        },
+        "Noto Color Emoji",
       }
+
+      -- Fix font rendering (https://github.com/wez/wezterm/issues/5990)
+      config.front_end = "WebGpu"
+
+      return config
     '';
   };
 }
