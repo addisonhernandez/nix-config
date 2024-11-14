f4e1eed9d05a6bf373197cafb94f0a1d5f95e8c3
Addison Hernandez <addison.hernandez@gmail.com>
2024-11-14 09:55:03 -0500

    feat(home/fish): Add functions and abbrs

--- home/addison/features/cli/fish.nix
+++ home/addison/features/cli/fish.nix
@@ -1,4 +1,4 @@
-{
+{...}: {
   programs.fish = {
     enable = true;
 
@@ -14,6 +14,50 @@
 
     functions = {
       fish_greeting = "";
+
+      lx = {
+        description = "List contents of a directory, including hidden, in long format with classification indicators";
+        wraps = "eza";
+        body = ''
+          eza \
+            --all \
+            --classify \
+            --git \
+            --group \
+            --group-directories-first \
+            --header \
+            --icons \
+            --long \
+            --smart-group \
+            --sort=type \
+            $argv
+        '';
+      };
+
+      lt = {
+        description = "List files in a tree (two levels deep by default)";
+        wraps = "lx";
+        body = "lx --tree --level=2 $argv";
+      };
+
+      tkdir = {
+        description = "Create a new directory and cd into it";
+        argumentNames = "dirname";
+        body = ''
+          set --function dirname (path resolve $dirname)
+          mkdir --parents $dirname
+          and cd $dirname
+        '';
+      };
+
+      multicd = "echo cd (string repeat --count (math (string length -- $argv[1]) -1) ../)";
+    };
+
+    shellAbbrs = {
+      dotdot = {
+        regex = "^\\.\\.+$";
+        function = "multicd";
+      };
     };
   };
 }
