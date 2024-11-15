53cd356396d33304f744283a668d9ce6d0201ef5
Addison Hernandez <addison.hernandez@gmail.com>
2024-11-15 09:05:56 -0500

    feat(kde): Enable XDG portal and set kde default

--- hosts/common/optional/kde.nix
+++ hosts/common/optional/kde.nix
@@ -2,7 +2,11 @@
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
   environment.plasma6.excludePackages = with pkgs; [
-    konsole
-    elisa
+    konsole # terminal
+    elisa # music player
   ];
+  xdg.portal = {
+    enable = true;
+    config.common.default = ["kde"];
+  };
 }
