b7dd7331a640b42f61d7402ebe5df9796209f9b7
Addison Hernandez <addison.hernandez@gmail.com>
2024-11-21 07:40:07 -0500

    feat(nix): Add custom flake registry and auto gc

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -1,7 +1,6 @@
 {
   inputs,
   lib,
-  pkgs,
   ...
 }: let
   flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
@@ -16,10 +15,21 @@ in {
         "flakes"
         "nix-command"
       ];
+      flake-registry = ""; # disable global flake registry
       trusted-users = [
         "root"
         "@wheel"
       ];
+      warn-dirty = false;
     };
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than +5"; # keep last 5 generations
+    };
+
+    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
   };
 }
