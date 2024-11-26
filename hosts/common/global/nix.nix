b2f0d666eae2400cae7c260e5d9265cefe4f0bd8
Addison Hernandez <addison.hernandez@gmail.com>
2024-11-26 06:37:52 -0500

    feat(nix): add nix-community cachix binary cache
    TODO: set up a local binary cache to share binaries between my local
    deployments

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -6,7 +6,16 @@
   flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
 in {
   nix = {
-    settings = {
+    settings = let
+      # jeevesCache = {
+      #   substituter = "nixcache.addisonhernandez.com";
+      #   publicKey = "";
+      # };
+      nixCommunityCache = {
+        substituter = "https://nix-community.cachix.org";
+        publicKey = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
+      };
+    in {
       auto-optimise-store = lib.mkDefault true;
       experimental-features = [
         "ca-derivations"
@@ -14,6 +23,14 @@ in {
         "nix-command"
       ];
       flake-registry = ""; # disable global flake registry
+
+      extra-substituters = [nixCommunityCache.substituter];
+      extra-trusted-public-keys = [nixCommunityCache.publicKey];
+
+      # TODO: set up a local binary cache on jeeves
+      # substituters = lib.mkBefore [jeevesCache.substituter];
+      # trusted-public-keys = lib.mkBefore [jeevesCache.publicKey];
+
       trusted-users = [
         "root"
         "@wheel"
@@ -24,7 +41,7 @@ in {
     gc = {
       automatic = true;
       dates = "weekly";
-      options = "--delete-older-than 14d";
+      options = "--delete-older-than 2w";
     };
 
     registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
