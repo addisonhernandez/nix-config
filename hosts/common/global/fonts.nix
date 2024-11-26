7c7e717cd60c0a5598b5c280654b8ab35db4695f
Addison Hernandez <addison.hernandez@gmail.com>
2024-11-26 06:47:41 -0500

    feat(fonts): add additional font config
    Added fonts:
      - Monospace / Programming:
        - Jetbrains Mono
        - Source Code Pro
      - General Use:
        - Lato
      - Symbols:
        - Font Awesome
      - Math:
        - Fira Math
        - LM Math
    
    TODO: add Joypixels (requires manually accepting unfree license)

--- hosts/common/global/fonts.nix
+++ hosts/common/global/fonts.nix
@@ -1,18 +1,35 @@
 {pkgs, ...}: {
   # Install some fonts
   fonts = {
+    # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/config/fonts/packages.nix#L34
     enableDefaultPackages = true;
 
     packages = with pkgs; [
+      # mono / programming
       fira-code-nerdfont
-      inter
+      jetbrains-mono
       monaspace
+      source-code-pro
+
+      # general use
+      inter
+      lato
+
+      # icons / symbols
+      font-awesome
+      # joypixels # requires manually accepting an unfree license
+
+      # math
+      fira-math
+      lmmath
     ];
 
     fontconfig = {
       defaultFonts = {
         monospace = ["FiraCode Nerd Font" "Source Code Pro" "Monospace"];
-        sansSerif = ["Inter" "DejaVu Sans"];
+        sansSerif = ["Inter" "Lato" "DejaVu Sans"];
+        serif = ["Noto Serif"];
+        emoji = ["Noto Color Emoji"];
       };
     };
   };
