b7964d95a78ac246da6d413b276883ffef517253
Addison Hernandez <addison.hernandez@gmail.com>
2024-12-27 08:19:00 -0500

    feat(home/wezterm): update font rendering
    Add Monaspace Radon as italic font

--- home/addison/features/desktop/wezterm/wezterm.lua
+++ home/addison/features/desktop/wezterm/wezterm.lua
@@ -1,9 +1,11 @@
+-- The following gets prepended automatically in the wezterm home-manager
+-- module. Uncomment it while editing in another context to fix warnings.
+-- local wezterm = require 'wezterm'
+
 local config = wezterm.config_builder()
 
 config.color_scheme = "Catppuccin Macchiato"
 
--- Fix font rendering (https://github.com/wez/wezterm/issues/5990)
-config.front_end = "WebGpu"
 config.font = wezterm.font_with_fallback {
   {
     family = "FiraCode Nerd Font",
@@ -11,5 +13,22 @@ config.font = wezterm.font_with_fallback {
   },
   "Noto Color Emoji",
 }
+config.font_rules = {
+  -- Italic
+  {
+    italic = true,
+    font = wezterm.font_with_fallback {
+      {
+        family = "Monaspace Radon Var",
+        harfbuzz_features = { "calt", "liga" },
+      },
+    },
+  },
+}
+-- Fix font rendering (https://github.com/wez/wezterm/issues/5990)
+config.front_end = "WebGpu"
+
+config.hide_tab_bar_if_only_one_tab = true
+config.window_background_opacity = 0.95
 
 return config
