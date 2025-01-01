0cf7ae926b1652b1775b6eb245a94ce79d106f36
Addison Hernandez <addison.hernandez@gmail.com>
2024-12-31 22:57:42 -0500

    feat(kde): add wl-clipboard-rs
    wl-clipboard-rs provides wl_copy and wl_paste utilities to interface
    between applications and the wayland clipboard.
    
    TODO: separate this option into a wayland.nix config that can be shared
    between wayland desktop environments. For now I only use KDE, so it made
    sense for me to group the option with KDE options.

--- hosts/common/optional/kde.nix
+++ hosts/common/optional/kde.nix
@@ -1,12 +1,16 @@
 {pkgs, ...}: {
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
+
   environment.plasma6.excludePackages = with pkgs; [
     konsole # terminal
     elisa # music player
   ];
+
   xdg.portal = {
     enable = true;
     config.common.default = ["kde"];
   };
+
+  environment.systemPackages = [pkgs.wl-clipboard-rs];
 }
