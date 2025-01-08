224567d7c5587fa1d4c1963b0b3cd918e0360e83
Addison Hernandez <addison.hernandez@gmail.com>
2025-01-08 13:34:05 -0500

    chore(jeeves/intel-graphics-drivers): remove cruft

--- hosts/jeeves/intel-graphics-drivers.nix
+++ hosts/jeeves/intel-graphics-drivers.nix
@@ -2,20 +2,14 @@
 {
   imports = [ inputs.hardware.nixosModules.common-gpu-intel ];
 
-  hardware.intelgpu.vaapiDriver = "intel-media-driver";
-  hardware.intelgpu.enableHybridCodec = true;
-
-  # enable VAAPI and Intel QSV
-  # nixpkgs.config.packageOverrides = pkgs: {
-  #   vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
-  # };
-  hardware.graphics = {
-    enable = true;
-    extraPackages = with pkgs; [
-      # intel-media-driver
-      libva-vdpau-driver
-      # intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
-      # vpl-gpu-rt # QSV on 11th gen or newer
-    ];
+  hardware = {
+    intelgpu = {
+      vaapiDriver = "intel-media-driver";
+      enableHybridCodec = true;
+    };
+    graphics = {
+      enable = true;
+      extraPackages = with pkgs; [ libva-vdpau-driver ];
+    };
   };
 }
