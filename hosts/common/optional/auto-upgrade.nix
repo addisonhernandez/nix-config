e42547a198a131cfc74ac2c7790e882a49891dbc
Addison Hernandez <addison.hernandez@gmail.com>
2025-02-09 08:44:35 -0500

    auto-upgrade: prefer FLAKE environment var
    Defaults to codeberg repository as a fallback

--- hosts/common/optional/auto-upgrade.nix
+++ hosts/common/optional/auto-upgrade.nix
@@ -1,10 +1,14 @@
+let
+  defaultStr = default: maybeStr: if maybeStr != "" then maybeStr else default;
+  flake = defaultStr "git+https://codeberg.org/addison/nix-config" (builtins.getEnv "FLAKE");
+in
 {
   system.autoUpgrade = {
     enable = true;
 
+    inherit flake;
     allowReboot = true;
     dates = "daily";
-    flake = "git+https://codeberg.org/addison/nix-config";
     operation = "boot";
     persistent = true;
     rebootWindow.lower = "01:00";
