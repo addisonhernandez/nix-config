a01c70e7e726b61fe181b78d02879bda74f87242
Addison Hernandez <addison.hernandez@gmail.com>
2025-02-09 07:19:03 -0500

    home/tldr: enable systemd service to update cache
    This is currently enabled in addition to the module option to update on
    invocation once a week.
    
    The built-in update feature is annoying because it is blocking, and takes
    a non-trivial amount of time to complete before outputting the requested
    tldr document. This service should update the cache periodically in the
    background, which should remove the need to wait for the cache to update
    before giving the desired output.

--- home/addison/features/cli/tldr.nix
+++ home/addison/features/cli/tldr.nix
@@ -1,9 +1,31 @@
+{ lib, pkgs, ... }:
 {
   programs.tealdeer = {
     enable = true;
     settings.updates = {
       auto_update = true;
       auto_update_interval_hours = 120;
+    };
+  };
+
+  systemd.user = {
+    services.tldr-update = {
+      Unit = {
+        Description = "tldr cache update";
+        After = "network-online.target";
+        Wants = "network-online.target";
+      };
+      Service = {
+        Type = "oneshot";
+        ExecStart = ''${lib.getExe pkgs.tealdeer} --update'';
+      };
+    };
+
+    timers.tldr-update = {
+      Timer = {
+        OnCalendar = "daily";
+        Persistent = true;
+      };
     };
   };
 }
