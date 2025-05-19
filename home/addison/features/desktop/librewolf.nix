a71f1398d99c838ab2dfd0e3dc2346e668faeabb
Addison Hernandez <addison.hernandez@gmail.com>
2025-05-19 14:42:06 -0400

    nixos: version update 25.05 -> 25.11
    Notable changes:
    
    - aardvark-dns:   1.14.0 -> 1.15.0
    - curl:           8.12.1 -> 8.13.0
    - ell:            0.73 -> 0.76
    - glib:           2.82.5 -> 2.84.1
    - gtk4:           4.16.12 -> 4.18.5
    - libadwaita:     1.6.4 -> 1.7.2
    - mkpasswd:       5.5.23 -> 5.6.0
    - nix:            2.24.14 -> 2.28.3
    - signal-desktop: 7.52.0 -> 7.54.0
    - proton:         GE-Proton_9.27 -> GE-Proton_10.1

--- home/addison/features/desktop/librewolf.nix
+++ home/addison/features/desktop/librewolf.nix
@@ -3,10 +3,34 @@
     enable = true;
 
     settings = {
-      "privacy.resistFingerprinting.letterboxing" = true;
-      "privacy.clearOnShutdown.history" = false;
-      "privacy.clearOnShutdown.downloads" = false;
+      # Appearance
+      "browser.compactmode.show" = true;
+      "browser.display.use_system_colors" = true;
+      "browser.download.autohideButton" = true;
+      "browser.toolbars.bookmarks.visibility" = "newtab";
+      "browser.uidensity" = 1; # compact
+      "font.default.x-wester" = "sans-serif";
+      "font.name.monospace.x-western" = "Maple Mono NF";
+      "font.name.sans-serif.x-wester" = "Inter";
+      "font.name.serif.x-western" = "Noto Serif";
+
+      # Functionality
+      "browser.gesture.swipe.left" = "";
+      "browser.gesture.swipe.right" = "";
+      "browser.search.openintab" = true;
+      "browser.urlbar.suggest.history" = false;
       "middlemouse.paste" = false;
+      "widget.use-xdg-desktop-portal.file-picker" = 1;
+
+      # Privacy
+      "browser.contentblocking.category" = "strict";
+      "browser.ml.enable" = false;
+      "privacy.clearOnShutdown.history" = false;
+      "privacy.fingerprintingProtection" = true;
+      "privacy.resistFingerprinting.letterboxing" = true;
+      "privacy.trackingprotection.emailtracking.enabled" = true;
+      "privacy.trackingprotection.enabled" = true;
+      "privacy.trackingprotection.socialtracking.enabled" = true;
     };
   };
 }
