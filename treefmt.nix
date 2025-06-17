c044d8b7128b5d63ec2a6848f8f4dcc1c0ca5c3f
Addison Hernandez <addison.hernandez@gmail.com>
2025-06-17 14:06:52 -0400

    style(treefmt): fix stylix lint warnings

--- treefmt.nix
+++ treefmt.nix
@@ -1,37 +1,42 @@
 {
   projectRootFile = "flake.nix";
-  settings.global = {
-    excludes = [
-      "LICENSE"
-      "*.example"
-    ];
-    on-unmatched = "info";
-  };
 
-  # Nix
-  programs.nixfmt.enable = true;
-  settings.formatter.nixfmt.options = [ "--strict" ];
-
-  # Lua
-  programs.stylua = {
-    enable = true;
-    settings = {
-      indent_type = "Spaces";
-      indent_width = 2;
+  settings = {
+    global = {
+      excludes = [
+        "LICENSE"
+        "*.example"
+      ];
+      on-unmatched = "info";
     };
+    formatter.nixfmt.options = [ "--strict" ];
   };
 
-  # Yaml
-  programs.yamlfmt = {
-    enable = true;
-    settings.formatter = {
-      type = "basic";
-      pad_line_comments = 2;
-      retain_line_breaks = true;
-      trim_trailing_whitespace = true;
+  programs = {
+    # Nix
+    nixfmt.enable = true;
+
+    # Lua
+    stylua = {
+      enable = true;
+      settings = {
+        indent_type = "Spaces";
+        indent_width = 2;
+      };
     };
-  };
 
-  # Just
-  programs.just.enable = true;
+    # Yaml
+    yamlfmt = {
+      enable = true;
+      settings.formatter = {
+        type = "basic";
+        pad_line_comments = 2;
+        retain_line_breaks = true;
+        trim_trailing_whitespace = true;
+      };
+    };
+
+    # Just
+    just.enable = true;
+  };
 }
