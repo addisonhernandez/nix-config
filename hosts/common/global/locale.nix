ce190ea57f577e92e2c230b2c8b043a154720653
Addison Hernandez <addison.hernandez@gmail.com>
2025-06-27 05:38:50 -0400

    refactor(locale): set locale once and then reuse it

--- hosts/common/global/locale.nix
+++ hosts/common/global/locale.nix
@@ -1,19 +1,20 @@
+{ config, lib, ... }:
 {
   time.timeZone = "America/New_York";
 
   i18n = {
     defaultLocale = "en_US.UTF-8";
 
-    extraLocaleSettings = {
-      LC_ADDRESS = "en_US.UTF-8";
-      LC_IDENTIFICATION = "en_US.UTF-8";
-      LC_MEASUREMENT = "en_US.UTF-8";
-      LC_MONETARY = "en_US.UTF-8";
-      LC_NAME = "en_US.UTF-8";
-      LC_NUMERIC = "en_US.UTF-8";
-      LC_PAPER = "en_US.UTF-8";
-      LC_TELEPHONE = "en_US.UTF-8";
-      LC_TIME = "en_US.UTF-8";
-    };
+    extraLocaleSettings = lib.genAttrs [
+      "LC_ADDRESS"
+      "LC_IDENTIFICATION"
+      "LC_MEASUREMENT"
+      "LC_MONETARY"
+      "LC_NAME"
+      "LC_NUMERIC"
+      "LC_PAPER"
+      "LC_TELEPHONE"
+      "LC_TIME"
+    ] (_: config.i18n.defaultLocale);
   };
 }
