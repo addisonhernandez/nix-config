e3d0ee99466aed2446bcc98ddfac9fb08ca6bd79
Addison Hernandez <addison.hernandez@gmail.com>
2025-07-12 06:32:41 -0400

    tailscale: add authkey to register tailnet nodes

--- hosts/common/global/tailscale.nix
+++ hosts/common/global/tailscale.nix
@@ -1,11 +1,18 @@
-{ lib, ... }:
+{ config, lib, ... }:
 {
   services.tailscale = {
     enable = true;
+
+    # [TODO] use sops module to generate authKeyFile path
+    authKeyFile = "/run/secrets/services/tailscale/nixos-authkey";
+
+    extraUpFlags = [ "--ssh" ];
+    openFirewall = true;
+
+    # client | both => set reverse path filtering to loose
+    # server | both => enable IP forwarding
     useRoutingFeatures = lib.mkDefault "client";
-    extraSetFlags = [ "--ssh" ];
   };
 
-  # Facilitate firewall punching
-  # networking.firewall.allowedUDPPorts = [41641];
+  networking.firewall.trustedInterfaces = [ config.services.tailscale.interfaceName ];
 }
