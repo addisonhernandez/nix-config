65e3c2a818b6dae741bbabaa73cd01e032991989
Addison Hernandez <addison.hernandez@gmail.com>
2025-07-19 16:40:34 -0400

    fix(nix-ssh-serve): fixup public key path

--- hosts/common/optional/nix-ssh-serve.nix
+++ hosts/common/optional/nix-ssh-serve.nix
@@ -2,7 +2,7 @@
 {
   nix.sshServe = {
     enable = true;
-    keys = [ (builtins.readFile "${inputs.secrets}/users/nix-ssh/public_keys/id/nix-ssh.pub") ];
+    keys = [ (builtins.readFile "${inputs.secrets}/users/nix-ssh/public_keys/id/nix-ssh_ed25519.pub") ];
     protocol = "ssh-ng";
     trusted = true; # add nix-ssh user to `nix.settings.trusted-users`
     write = true;
