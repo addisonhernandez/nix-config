19d75bd5295daa18b9e510bec3f9daa2e2d84a7b
Addison Hernandez <addison.hernandez@gmail.com>
2025-07-29 10:17:15 -0400

    home/helix: update nix LSP nixfmt arguments

--- home/addison/features/cli/helix/languages.nix
+++ home/addison/features/cli/helix/languages.nix
@@ -11,32 +11,40 @@
           "nixd"
           "nil"
         ];
+        auto-format = true;
+        formatter.command = "nixfmt";
+        formatter.args = [
+          "--strict"
+          "--width=80"
+        ];
       }
     ];
 
-    language-server = {
-      fish-lsp = {
-        command = "fish-lsp";
-        args = [ "start" ];
-      };
-      nil = {
-        command = "nil";
-        settings.formatting.command = [
+    language-server =
+      let
+        nixfmt.command = [
           "nixfmt"
           "--strict"
+          "--width=80"
         ];
+      in
+      {
+        fish-lsp = {
+          command = "fish-lsp";
+          args = [ "start" ];
+        };
+        nil = {
+          command = "nil";
+          settings.formatting = { inherit (nixfmt) command; };
+        };
+        nixd = {
+          command = "nixd";
+          args = [
+            "--log=error"
+            "--inlay-hints"
+          ];
+          settings.formatting = { inherit (nixfmt) command; };
+        };
       };
-      nixd = {
-        command = "nixd";
-        args = [
-          "--log=error"
-          "--inlay-hints"
-        ];
-        settings.formatting.command = [
-          "nixfmt"
-          "--strict"
-        ];
-      };
-    };
   };
 }
