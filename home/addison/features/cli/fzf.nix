c19c131fd25d92e94ab68dcd8c60f01dd2b430a4
Addison Hernandez <addison.hernandez@gmail.com>
2025-08-09 07:28:45 -0400

    fzf: ignore dev and build artifact directories

--- home/addison/features/cli/fzf.nix
+++ home/addison/features/cli/fzf.nix
@@ -1,4 +1,8 @@
 {
   programs.fzf.enable = true;
+  programs.fzf.defaultOptions = [
+    # Ignore dev and build artifact directories
+    "--walker-skip=.git,node_modules,.direnv,result"
+  ];
   catppuccin.fzf.enable = true;
 }
