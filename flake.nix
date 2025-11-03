61fe2b8e72c608fe0740e0fb6a43b8a6658aa879
Addison Hernandez <addison.hernandez@gmail.com>
2025-11-03 06:08:44 -0500

    Update flake inputs for flake-parts & dendritic

--- flake.nix
+++ flake.nix
@@ -2,16 +2,23 @@
   description = "NixOS and Home Manager Config Entrypoint";
 
   inputs = {
+    ## Configuration building blocks
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs.follows = "nixpkgs-unstable";
 
-    systems.url = "github:nix-systems/x86_64-linux";
-    nixos-hardware.url = "github:nixos/nixos-hardware";
-
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixos-hardware.url = "github:nixos/nixos-hardware";
+    systems.url = "github:nix-systems/x86_64-linux";
+
+    ## Dendritic pattern inputs
+    flake-aspects.url = "github:vic/flake-aspects";
+    import-tree.url = "github:vic/import-tree";
+
+    ## Flake inputs
     agenix.url = "github:ryantm/agenix";
     agenix.inputs = {
       nixpkgs.follows = "nixpkgs";
@@ -27,10 +34,17 @@
     nix-index-db.inputs.nixpkgs.follows = "nixpkgs";
 
     secrets.url = "git+ssh://git@codeberg.org/addison/secrets.git?ref=main&shallow=1";
-    secrets.inputs.agenix.follows = "";
+    secrets.inputs.agenix.follows = "agenix";
+
+    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
 
     treefmt-nix.url = "github:numtide/treefmt-nix";
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+
+    ## Non-flake inputs
+    # <name>.url = "...";
+    # <name.flake = false;
   };
 
   outputs =
