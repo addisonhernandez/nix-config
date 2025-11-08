6319ae8aa97d99ac92b85a7456f197b5a862d5b2
Addison Hernandez <addison.hernandez@gmail.com>
2025-11-08 10:45:50 -0500

    lib: remove redundant `forEachSystem` function
    The functionality is included in flake-parts.
    
    Also leave some todo notes for my future self.

--- modules/flake/configurations.nix
+++ modules/flake/configurations.nix
@@ -6,7 +6,8 @@
       hostnames = [
         "greenbeen" # Mini Desktop (Beelink SER7)
         "hedgehog" # Laptop (Dell XPS 9560)
-        "iso" # Custom installer image
+        # [todo] Package the ISO image separately, it's not actually a host.
+        # "iso" # Custom installer image
         "jeeves" # Media Server (Beelink Mini S12 Pro)
         "vulcan" # Desktop
       ];
