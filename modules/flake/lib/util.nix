8139f9a3c98cc177102109cb828b3035ac3d18c3
Addison Hernandez <addison.hernandez@gmail.com>
2025-11-08 10:45:50 -0500

    lib: remove redundant `forEachSystem` function
    The functionality is included in flake-parts.
    
    Also leave some todo notes for my future self.

--- modules/flake/lib/util.nix
+++ modules/flake/lib/util.nix
@@ -1,16 +1,10 @@
 { inputs, lib, ... }:
-let
-  pkgsFor = lib.genAttrs (import inputs.systems) (
+{
+  flake.lib.pkgsFor = lib.genAttrs (import inputs.systems) (
     system:
     import inputs.nixpkgs {
       inherit system;
       config.allowUnfree = true;
     }
   );
-
-  forEachSystem =
-    f: lib.genAttrs (import inputs.systems) (system: f pkgsFor.${system});
-in
-{
-  flake.lib = { inherit forEachSystem pkgsFor; };
 }
