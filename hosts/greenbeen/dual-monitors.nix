f78c58e401486431ce1314240f20749f59c57ef9
Addison Hernandez <addison.hernandez@gmail.com>
2026-01-19 11:05:17 -0500

    New monitor, who dis?

--- hosts/greenbeen/dual-monitors.nix
+++ hosts/greenbeen/dual-monitors.nix
@@ -1,13 +1,13 @@
 { lib, pkgs, ... }:
 {
   # Configure dual monitors
-  # HDMI: primary
-  # DP: secondary on the left and rotated with left edge up
-  services.xserver.displayManager.setupCommands =
-    # sh
-    ''
-      ${lib.getExe pkgs.xorg.xrandr} \
-        --output DP-1 --mode 1920x1080 --pos 0x0 --rotate left \
-        --output HDMI-A-1 --primary --mode 1920x1080 --pos 1080x250
-    '';
+  # DP: primary
+  # HDMI: secondary on the left and rotated with left edge up
+  services.xserver.displayManager = {
+    setupCommands = builtins.concatStringsSep " " [
+      (lib.getExe pkgs.xorg.xrandr)
+      "--output HDMI-A-1 --mode 1920x1080 --pos 0x0 --rotate left"
+      "--output DP-1 --primary --mode 1920x1080 --pos 1080x0"
+    ];
+  };
 }
