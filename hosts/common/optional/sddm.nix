9a7f484ca478758c0edf2cb841c0d5ba99bb9f3e
Addison Hernandez <addison.hernandez@gmail.com>
2026-03-21 19:50:20 -0400

    sddm: customize background image

--- hosts/common/optional/sddm.nix
+++ hosts/common/optional/sddm.nix
@@ -1,6 +1,20 @@
+{ pkgs, ... }:
 {
   services.displayManager.sddm = {
     enable = true;
     autoNumlock = true;
   };
+
+  # Specify an SDDM background with a user-override config.
+  # pkgs.writeTextDir accepts a path and some contents to write at that path.
+  # This file can be found at runtime at /run/current-system/sw/<path>.
+  environment.systemPackages = [
+    (pkgs.writeTextDir "share/sddm/themes/breeze/theme.conf.user"
+      # ini
+      ''
+        [General]
+        background=${../assets/synthwave-office.jpg}
+      ''
+    )
+  ];
 }
