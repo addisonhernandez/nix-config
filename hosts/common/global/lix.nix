2313b02878e2a73fe894576119378a879c4b8e05
Addison Hernandez <addison.hernandez@gmail.com>
2026-04-06 10:29:55 -0400

    lix: auto-convert ints to strings

--- hosts/common/global/lix.nix
+++ hosts/common/global/lix.nix
@@ -4,6 +4,8 @@
     package = pkgs.lixPackageSets.latest.lix;
     settings = {
       experimental-features = [
+        # Allow "v${version}" instead of "v${toString version}"
+        "coerce-integers"
         # Nix uses "pipe-operators" (plural)
         "pipe-operator"
       ];
