5a9c87a416e2371aef9b5b56088083269df34802
Addison Hernandez <addison.hernandez@gmail.com>
2026-06-08 17:57:09 -0400

    Some prefs are locked, some are just defaults

--- home/addison/features/desktop/zen-browser.nix
+++ home/addison/features/desktop/zen-browser.nix
@@ -6,41 +6,49 @@
   ...
 }:
 let
-  prefs = {
-    "browser.aboutConfig.showWarning" = false;
-    "browser.backup.location" =
-      "${config.home.homeDirectory}/Documents/backup/Zen Browser";
-    "browser.compactmode.show" = true;
-    "browser.gesture.swipe.left" = "";
-    "browser.gesture.swipe.right" = "";
-    "browser.ml.enable" = false;
-    "browser.ml.chat.enable" = false;
-    "browser.ml.pageAssist.enable" = false;
-    "browser.newtabpage.activity-stream.showSponsored" = false;
-    "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
-    "browser.newtabpage.activity-stream.system.showSponsored" = false;
-    "browser.uidensity" = 1; # 0->Normal 1->Compact 2->Touch
-    "browser.urlbar.sponsoredTopSites" = false;
-    "browser.urlbar.trimHttps" = false;
-    "browser.urlbar.trimURLs" = false;
+  defaultPrefs =
+    lib.mapAttrsToList
+      (name: value: "pref(${lib.toJSON name}, ${lib.toJSON value});")
+      {
+        "browser.backup.location" =
+          "${config.home.homeDirectory}/Documents/backup/Zen Browser";
+        "browser.gesture.swipe.left" = "";
+        "browser.gesture.swipe.right" = "";
+        "browser.uidensity" = 1; # 0->Normal 1->Compact 2->Touch
+        "browser.urlbar.trimHttps" = false;
+        "browser.urlbar.trimURLs" = false;
 
-    "font.default.x-western" = "sans-serif";
-    "font.name.monospace.x-western" = "Maple Mono NF";
-    "font.name.sans-serif.x-western" = "Inter";
-    "font.name.serif.x-western" = "Noto Serif";
+        "font.default.x-western" = "sans-serif";
+        "font.name.monospace.x-western" = "Maple Mono NF";
+        "font.name.sans-serif.x-western" = "Inter";
+        "font.name.serif.x-western" = "Noto Serif";
 
-    "middlemouse.paste" = false;
+        "widget.use-xdg-desktop-portal.file-picker" = 1;
 
-    "widget.use-xdg-desktop-portal.file-picker" = 1;
+        "zen.tabs.vertical.right-side" = true;
+        "zen.theme.accent-color" = "#c6a0f6"; # catppuccin-macchiato mauve
+        "zen.view.compact.enable-at-startup" = true;
+        "zen.view.compact.hide-tabbar" = true;
+        "zen.view.compact.hide-toolbar" = true;
+        "zen.view.compact.should-enable-at-startup" = true;
+        "zen.view.use-single-toolbar" = false;
+      };
+  lockedPrefs =
+    lib.mapAttrsToList
+      (name: value: "lockedPref(${lib.toJSON name}, ${lib.toJSON value});")
+      {
+        "browser.aboutConfig.showWarning" = false;
+        "browser.compactmode.show" = true;
+        "browser.ml.enable" = false;
+        "browser.ml.chat.enable" = false;
+        "browser.ml.pageAssist.enable" = false;
+        "browser.newtabpage.activity-stream.showSponsored" = false;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+        "browser.newtabpage.activity-stream.system.showSponsored" = false;
+        "browser.urlbar.sponsoredTopSites" = false;
 
-    "zen.tabs.vertical.right-side" = true;
-    "zen.theme.accent-color" = "#c6a0f6"; # catppuccin-macchiato mauve
-    "zen.view.compact.enable-at-startup" = true;
-    "zen.view.compact.hide-tabbar" = true;
-    "zen.view.compact.hide-toolbar" = true;
-    "zen.view.compact.should-enable-at-startup" = true;
-    "zen.view.use-single-toolbar" = false;
-  };
+        "middlemouse.paste" = false;
+      };
 
   defaultExtensions =
     let
@@ -75,71 +83,63 @@ let
       (mkExt "styl-us" "{7a7a4a92-a2a0-41d1-9fd7-1e92480d612d}" { })
     ];
 
-  defaultSearchEngines =
-    let
-      params = lib.concatStringsSep "&";
-    in
-    [
-      {
-        Name = "DuckDuckGo NoAI";
-        URLTemplate = "https://noai.duckduckgo.com/?${
-          params [
-            # See: https://duckduckgo.com/duckduckgo-help-pages/settings/params
-            "k1=-1" # No Advertisements
-            "kl=us-en" # Region
-            "kp=-2" # No Safe Search
-            "q={searchTerms}"
-          ]
-        }";
-        IconURL = "https://duckduckgo.com/favicon.ico";
-        Alias = "@ddg";
-      }
-      {
-        Name = "Nix Packages";
-        URLTemplate = "https://search.nixos.org/packages?${
-          params [
-            "channel=unstable"
-            "query={searchTerms}"
-          ]
-        }";
-        IconURL = "https://wiki.nixos.org/favicon.ico";
-        Alias = "@np";
-      }
-      {
-        Name = "NixOS Options";
-        URLTemplate = "https://search.nixos.org/options?${
-          params [
-            "channel=unstable"
-            "query={searchTerms}"
-          ]
-        }";
-        IconURL = "https://wiki.nixos.org/favicon.ico";
-        Alias = "@no";
-      }
-      {
-        Name = "NixOS Wiki";
-        URLTemplate = "https://wiki.nixos.org/w/index.php?search={searchTerms}";
-        IconURL = "https://wiki.nixos.org/favicon.ico";
-        Alias = "@nw";
-      }
-      {
-        Name = "Arch Wiki";
-        URLTemplate = "https://wiki.archlinux.org/index.php?search={searchTerms}";
-        IconURL = "https://wiki.archlinux.org/favicon.ico";
-        Alias = "@aw";
-      }
-    ];
+  defaultSearchEngines = [
+    {
+      Name = "DuckDuckGo NoAI";
+      URLTemplate = "https://noai.duckduckgo.com/?${
+        lib.concatStringsSep "&" [
+          # See: https://duckduckgo.com/duckduckgo-help-pages/settings/params
+          "k1=-1" # No Advertisements
+          "kl=us-en" # Region
+          "kp=-2" # No Safe Search
+          "q={searchTerms}"
+        ]
+      }";
+      IconURL = "https://duckduckgo.com/favicon.ico";
+      Alias = "@ddg";
+    }
+    {
+      Name = "Nix Packages";
+      URLTemplate = "https://search.nixos.org/packages?${
+        lib.concatStringsSep "&" [
+          "channel=unstable"
+          "query={searchTerms}"
+        ]
+      }";
+      IconURL = "https://wiki.nixos.org/favicon.ico";
+      Alias = "@np";
+    }
+    {
+      Name = "NixOS Options";
+      URLTemplate = "https://search.nixos.org/options?${
+        lib.concatStringsSep "&" [
+          "channel=unstable"
+          "query={searchTerms}"
+        ]
+      }";
+      IconURL = "https://wiki.nixos.org/favicon.ico";
+      Alias = "@no";
+    }
+    {
+      Name = "NixOS Wiki";
+      URLTemplate = "https://wiki.nixos.org/w/index.php?search={searchTerms}";
+      IconURL = "https://wiki.nixos.org/favicon.ico";
+      Alias = "@nw";
+    }
+    {
+      Name = "Arch Wiki";
+      URLTemplate = "https://wiki.archlinux.org/index.php?search={searchTerms}";
+      IconURL = "https://wiki.archlinux.org/favicon.ico";
+      Alias = "@aw";
+    }
+  ];
 in
 {
   home.packages = [
     (pkgs.wrapFirefox
       inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.zen-browser-unwrapped
       {
-        extraPrefs = lib.concatLines (
-          lib.mapAttrsToList (
-            name: value: "lockPref(${lib.toJSON name}, ${lib.toJSON value});"
-          ) prefs
-        );
+        extraPrefs = lib.concatLines (defaultPrefs ++ lockedPrefs);
 
         extraPolicies = {
           AutofillAddressEnabled = false;
