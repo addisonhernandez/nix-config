32ea06e91878952f3b59576b14f74e873b3177ec
Addison Hernandez <addison.hernandez@gmail.com>
2026-08-01 07:46:41 -0400

    Add hostsnames to flake outputs

--- modules/flake/configurations.nix
+++ modules/flake/configurations.nix
@@ -1,29 +1,28 @@
-# [fixme] relocate to @/modules/
 { self, lib, ... }:
-{
-  flake =
-    let
-      hostnames = [
-        "greenbeen" # Mini Desktop (Beelink SER7)
-        "hedgehog" # Laptop (Dell XPS 9560)
-        # "iso" # Custom installer image
-        "jeeves" # Media Server (Beelink Mini S12 Pro)
-        "vulcan" # Desktop
-      ];
-      usernames = [
-        "addison"
-        "audrey"
-      ];
-      userHostPairs = lib.cartesianProduct {
-        user = usernames;
-        host = hostnames;
-      };
+let
+  hostnames = [
+    "greenbeen" # Mini Desktop (Beelink SER7)
+    "hedgehog" # Laptop (Dell XPS 9560)
+    # "iso" # Custom installer image
+    "jeeves" # Media Server (Beelink Mini S12 Pro)
+    "vulcan" # Desktop
+  ];
+  usernames = [
+    "addison"
+    "audrey"
+  ];
+  userHostPairs = lib.cartesianProduct {
+    user = usernames;
+    host = hostnames;
+  };
 
-      forEachHost = lib.genAttrs (hostnames ++ [ "iso" ]);
-      forEachHome = f: lib.mergeAttrsList (map f userHostPairs);
-    in
-    {
-      nixosConfigurations = forEachHost self.lib.mkHostConfig;
-      homeConfigurations = forEachHome self.lib.mkHomeConfig;
-    };
+  forEachHost = lib.genAttrs (hostnames ++ [ "iso" ]);
+  forEachHome = f: lib.mergeAttrsList (map f userHostPairs);
+in
+{
+  flake = {
+    inherit hostnames;
+    nixosConfigurations = forEachHost self.lib.mkHostConfig;
+    homeConfigurations = forEachHome self.lib.mkHomeConfig;
+  };
 }
